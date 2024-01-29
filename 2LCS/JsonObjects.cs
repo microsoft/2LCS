59943a4f4de030bf70e9404a06615c94a53a049c
Florian Hopfner <florian.hopfner@inway.de>
2024-01-29 18:56:55 +0100

    Update LCS JSON object class for assets

--- 2LCS/JsonObjects.cs
+++ 2LCS/JsonObjects.cs
@@ -6,21 +6,42 @@ namespace LCS.JsonObjects
     public class Asset
     {
         public string Id { get; set; }
-        public string Name { get; set; }
         public string FileName { get; set; }
         public string FileDescription { get; set; }
-        public int SizeByte { get; set; }
+        public int CommitStatus { get; set; }
         public int State { get; set; }
         public int Scope { get; set; }
-        public int CommitStatus { get; set; }
-        public string FileLocation { get; set; }
-        public int FileType { get; set; }
-        public string ModifiedDate { get; set; }
-        public string CreatedDate { get; set; }
         public int Version { get; set; }
-        public bool IsMockData { get; set; }
-        public string ParentFileAssetId { get; set; }
-        public string ArtifactId { get; set; }
+        public int FileType { get; set; }
+        public string CreatedByName { get; set; }
+        public string ModifiedByName { get; set; }
+        public bool IsGoldBuild { get; set; }
+        public int ValidationStatus { get; set; }
+        public string AadTenantId { get; set; }
+        public string ValidationStatusText { get; set; }
+        public string ReleaseDetailsAssetId { get; set; }
+        public string ReleaseDetailsLink { get; set; }
+        public string ParentAssetId { get; set; }
+        public bool IsInvalid { get; set; }
+        public string DisplayValidationSymbol { get; set; }
+        public string DisplayModifiedDate { get; set; }
+        public string DisplayCreatedDate { get; set; }
+        public string DisplayId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DisplayScope { get; set; }
+        public string DisplayState { get; set; }
+        public string Location { get; set; }
+        public bool IsAbsoluteLocation { get; set; }
+        public bool CanDelete { get; set; }
+        public bool CanUpdate { get; set; }
+        public bool CanPublish { get; set; }
+        public bool CanNavigate { get; set; }
+        public string DisplayVersion { get; set; }
+        public int OriginalScope { get; set; }
+        public string DisplayOriginalScope { get; set; }
+        public string LocalCreatedDate { get; set; }
+        public string LocalModifiedDate { get; set; }
     }
 
     public class AssetData
