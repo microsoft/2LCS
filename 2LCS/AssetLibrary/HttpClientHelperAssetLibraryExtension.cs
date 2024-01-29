76c363ceadc4259ba5586257327b3b81f71aade7
Florian Hopfner <florian.hopfner@inway.de>
2024-01-29 18:55:51 +0100

    Add GetSharedAssetVersionList request to asset library HttpClientHelper extension

--- 2LCS/AssetLibrary/HttpClientHelperAssetLibraryExtension.cs
+++ 2LCS/AssetLibrary/HttpClientHelperAssetLibraryExtension.cs
@@ -3,18 +3,28 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 
 namespace LCS.AssetLibrary;
 
 internal static class HttpClientHelperAssetLibraryExtension
 {
-    internal static string GetGetSharedAssetsUrl(this HttpClientHelper httpClientHelper,
+    internal static string GetGetSharedAssetsUrl(
+        this HttpClientHelper httpClientHelper,
         AssetFileType assetFileType)
     {
         return $"{httpClientHelper.LcsUrl}/FileAsset/GetSharedAssets/?assetKind={((int)assetFileType)}&_={DateTimeOffset.Now.ToUnixTimeSeconds()}";
     }
 
-    internal static List<Asset> GetSharedAssetList(this HttpClientHelper httpClientHelper,
+    internal static string GetGetFileAssetVersionsUrl(
+        this HttpClientHelper httpClientHelper)
+    {
+        return $"{httpClientHelper.LcsUrl}/FileAsset/GetFileAssetVersions";
+    }
+
+    internal static List<Asset> GetSharedAssetList(
+        this HttpClientHelper httpClientHelper,
         AssetFileType assetFileType)
     {
         var assetList = new List<Asset>();
@@ -25,16 +35,41 @@ internal static class HttpClientHelperAssetLibraryExtension
 
         var responseBody = result.Content.ReadAsStringAsync().Result;
         var response = JsonConvert.DeserializeObject<Response>(responseBody);
-        if (response.Success && response.Data != null)
+        if (response.Success 
+            && response.Data != null
+            && response.Data is JToken responseData)
         {
-            var responseData = response.Data as JToken;
-            if (responseData != null)
-            {
-                var assetData = responseData[0].ToObject<AssetData>();
-                assetList = assetData.Assets;
-            }
+            var assetData = responseData[0].ToObject<AssetData>();
+            assetList = assetData.Assets;
         }
 
         return assetList;
+    }
+
+    internal static List<AssetVersion> GetSharedAssetVersionList(
+        this HttpClientHelper httpClientHelper,
+        string parentFileAssetId)
+    {
+        var assetVersionList = new List<AssetVersion>();
+
+        var url = httpClientHelper.GetGetFileAssetVersionsUrl();
+        var stringContent = new StringContent(
+            $"parentFileAssetId={parentFileAssetId}",
+            Encoding.UTF8,
+            "application/x-www-form-urlencoded");
+        httpClientHelper.SetRequestVerificationToken($"{httpClientHelper.LcsUrl}/V2");
+        var result = httpClientHelper.GetHttpClient().PostAsync(url, stringContent).Result;
+        result.EnsureSuccessStatusCode();
+    
+        var responseBody = result.Content.ReadAsStringAsync().Result;
+        var response = JsonConvert.DeserializeObject<Response>(responseBody);
+        if (response.Success 
+            && response.Data != null
+            && response.Data is JToken responseData)
+        {
+            assetVersionList = responseData.ToObject<List<AssetVersion>>();
+        }
+
+        return assetVersionList;
     }
 }
